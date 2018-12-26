4f739e40e21ddf92ce883e602d768f5ef28df361
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2018-12-26 00:05:07 -0300

    [NEW] Creating IoC project

---
+++ eShop.DI/StartupDI.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using eShop.Persistence;
+using eShop.Domain.Interfaces;
+
+namespace eShop.DI
+{
+    public class StartupDI
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddSingleton(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
